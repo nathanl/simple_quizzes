09931c13ca6161ae02164dafbb693d0ff7bfd9a7
Nathan Long <him@nathanmlong.com>
2021-08-09 20:30:44 -0400

    Random messages and clips

--- addition_quiz.exs
+++ addition_quiz.exs
@@ -2,24 +2,20 @@ defmodule SpeedQuiz do
   # Numbers to be added will fall in this range
   @range 1..10
 
-  # Each answer's total won't be more than this
-  @max_total 15
-
   # Ask this many questions
   @question_count 5
 
   def run do
-    questions = generate_questions(@range, @max_total)
+    questions = generate_questions(@range)
     start_time = DateTime.utc_now()
 
     run_round(questions, start_time)
   end
 
-  defp generate_questions(num_range, max_total) do
+  defp generate_questions(num_range) do
     for i1 <- num_range, i2 <- num_range do
       [i1, i2]
     end
-    |> Enum.filter(fn [i1, i2] -> i1 + i2 <= max_total end)
     |> Enum.shuffle()
     |> Enum.take(@question_count)
   end
@@ -44,10 +40,15 @@ defmodule SpeedQuiz do
 
     unit = if average == 1, do: "second", else: "seconds"
 
+    clip_task = play_random_victory_clip()
+    victory_message = random_victory_message()
+
     IO.puts("""
-    You win! 😀 ❤️  🎆
+    #{victory_message}
     It took you #{elapsed} seconds to answer #{@question_count} questions - an average of #{average} #{unit} per question.
     """)
+
+    Task.await(clip_task)
   end
 
   defp parse_to_num(str) do
@@ -57,6 +58,24 @@ defmodule SpeedQuiz do
       {i, ""} -> {:ok, i}
       _ -> :error
     end
+  end
+
+  defp random_victory_message do
+    "victory_messages.txt"
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.random()
+  end
+
+  defp play_random_victory_clip do
+    filename =
+      "victory_clips"
+      |> File.ls!()
+      |> Enum.random()
+
+    Task.async(fn ->
+      System.cmd("afplay", ["victory_clips/#{filename}"])
+    end)
   end
 end
 
